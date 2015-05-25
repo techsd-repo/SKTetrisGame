a493390a8171a41fa544b7d8582865c2a26f85a1
techsd-repo <techlcsd@gmail.com>
2015-05-25 10:31:22 -0400

    Added row blast buy function, bug made, will be fixed

--- Tetris - Swift/RowBlastVC.swift
+++ Tetris - Swift/RowBlastVC.swift
@@ -14,14 +14,14 @@ class RowBlastVC: UIViewController {
     @IBOutlet weak var lv2label: UILabel!
     @IBOutlet weak var lv3label: UILabel!
     @IBOutlet weak var totalTokens: UILabel!
-    
+    var sd = ScoreDelegate()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+       totalTokens.text = String(sd.readTotalTokens())
         
         // Do any additional setup after loading the view.
     }
@@ -34,15 +34,24 @@ class RowBlastVC: UIViewController {
     
     
     @IBAction func prlevel1(sender: AnyObject) {
+        sd.setTotalTokens(sd.readTotalTokens() - 3)
+        sd.setrbLevel1(sd.readRB("1") + 1)
+        lv1label.text = String(sd.readRB("1"))
     }
     
     
   
     @IBAction func prlevel2(sender: AnyObject) {
+        sd.setTotalTokens(sd.readTotalTokens() - 6)
+        sd.setrbLevel2(sd.readRB("2") + 1)
+        lv2label.text = String(sd.readRB("2"))
     }
     
     
   
     @IBAction func prlevel3(sender: AnyObject) {
+        sd.setTotalTokens(sd.readTotalTokens() - 10)
+        sd.setrblevel3(sd.readRB("3") + 1)
+        lv3label.text = String(sd.readRB("3"))
     }
 }
