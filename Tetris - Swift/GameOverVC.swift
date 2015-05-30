046c583fb67e0082f6373a7986085bce12887d65
techsd-repo <techlcsd@gmail.com>
2015-05-30 19:05:13 -0400

    Added GameOverVC class

---
+++ Tetris - Swift/GameOverVC.swift
@@ -0,0 +1,16 @@
+//
+//  GameOverVC.swift
+//  Tetris - Swift
+//
+//  Created by TheTechsd on 5/30/15.
+//  Copyright (c) 2015 TheTechsd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameOverVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
